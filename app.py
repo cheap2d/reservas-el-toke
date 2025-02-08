2e7941ca2c3e4942dd09d69984d22649a9a6ee32
cheap2d <118389940+cheap2d@users.noreply.github.com>
2025-02-08 11:16:22 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -16,4 +16,6 @@ def webhook():
     """
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))  # Render asigna el puerto automáticamente
+    app.run(host="0.0.0.0", port=port, debug=True)  # Cambia host a 0.0.0.0
